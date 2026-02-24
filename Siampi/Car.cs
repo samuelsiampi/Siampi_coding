7945bfaa949178ab055feb448819f55b655243b9
josep <josep@CHRISTYMANBAWI>
2026-02-24 19:58:28 +1100

    Add project files.

---
+++ Siampi/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Siampi
+{
+    internal class Car
+    {
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public int Seats { get; set; }
+        public int Safetyrate { get; set; }
+
+    }
+}
---
+++ Siampi/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Runtime.Intrinsics.Arm;
+
+namespace Siampi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car siampiCar = new Car();
+    
+
+            siampiCar.Color = "Orange";
+            siampiCar.Model = "Chery";
+            siampiCar.Seats = 5;
+
+
+            Console.WriteLine($"Siampi's car color is {siampiCar.Color}.");
+            Console.WriteLine($"Siampi's car model is {siampiCar.Model}.");
+            Console.WriteLine($"Siampi's car seat has {siampiCar.Seats} seat.");
+
+            Car nunuCar = new Car();
+
+            nunuCar.Color = "Yellow";
+            nunuCar.Model = "Toyota";
+            nunuCar.Seats = 4;
+            nunuCar.Safetyrate = 5;
+
+
+            Console.WriteLine($"Nunu's car color is {nunuCar.Color}.");
+            Console.WriteLine($"Nunu's car model is {nunuCar.Model}.");
+            Console.WriteLine($"Nunu's car seat has {nunuCar.Seats} seat.");
+            Console.WriteLine($"Nunu's car safety rate is {nunuCar.Safetyrate} star.");
+
+            Car papaCar = new Car();
+
+            papaCar.Color = "Green";
+            papaCar.Model = "Honda";
+            papaCar.Seats = 7;
+            papaCar.Safetyrate = 3;
+
+
+            Console.WriteLine($"Papa's car color is {papaCar.Color}.");
+            Console.WriteLine($"Papa's car model is {papaCar.Model}.");
+            Console.WriteLine($"Papa's car seat has {papaCar.Seats} seat.");
+            Console.WriteLine($"Papa's car safety rate is {papaCar.Safetyrate} star.");
+
+          
+
+
+
+
+
+        }
+        
+    }    
+    
+}
